6ef15c95e74c4ccb506be331bb4f700a2bb2e8d3
AkinladeBola <leebola9@gmail.com>
2023-06-15 15:20:27 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main- main function of the program
+ * main - main function of the program
  *  
- * return- every program shuld always return 0(success)
+ * return: every program shuld always return 0(success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0); 
+	return (0);
 }
