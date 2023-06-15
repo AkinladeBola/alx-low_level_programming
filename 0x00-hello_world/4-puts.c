b5ae8eec16b69ae06585eb35b31089d64a29b9a7
AkinladeBola <leebola9@gmail.com>
2023-06-15 14:54:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * ALX
+ * is 
+ * challenging
+ * every program shuld always return 0 errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
