184840bab5558635add40700cfdd0efae039076a
AkinladeBola <leebola9@gmail.com>
2023-06-20 15:26:41 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * main - main function
+ *
+ * return: returns 0 when program is successful
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
